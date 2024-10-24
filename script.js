7af74346a7a4c05200f48ad161e207bfa404a5ee
MKcode-45 <kalotamishael@gmail.com>
2024-10-24 15:36:51 +0200

    Function to take human choice
    Created a function that takes the players choice
    by using a prompt to take the users input.
    toLowerCase is used to make the string case-
    insensitive.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //Created function to get computer choice
 //Function will choose three random numbers which are equal to rock paper or scissors
-
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random()*3);
     if(computerChoice === 0){
@@ -12,5 +11,11 @@ function getComputerChoice(){
    if(computerChoice === 2){
       return "scissors";
     }
+}
+  
+//A function that takes the human choice
+function getHumanChoice(){
+    let result = prompt("To play, choose rock, paper or scissors").toLocaleLowerCase();
+    return result;
 }
   
