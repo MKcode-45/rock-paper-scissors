dfbf86d5e5dfb80fc85c21476b06659a4b04b635
MKcode-45 <kalotamishael@gmail.com>
2024-10-24 15:58:57 +0200

    Added two new functions
    The gamePlay function runs the entire game and the
    playRound function contains the logic for a single
    round

--- script.js
+++ script.js
@@ -18,4 +18,48 @@ function getHumanChoice(){
     let result = prompt("To play, choose rock, paper or scissors").toLocaleLowerCase();
     return result;
 }
+
+//Game play function
+function playGame(){
+
+    //The two variables initialize human and computer scores
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    //Function runs a single round of the game
+    
+ function playRound(humanChoice, computerChoice) {
   
+   var win = `You win! ${humanChoice} beats ${computerChoice}`;
+   var lose = `You lose! ${computerChoice} beats ${humanChoice}`;
+
+   //The conditions determine the winner of each round
+   //Score is increased depending on the winner
+   
+   if (humanChoice === "rock" && computerChoice === "paper"){
+     ++computerScore;
+     console.log(lose);
+     } else if (humanChoice === "rock" && computerChoice === "scissors"){
+     ++humanScore;
+     console.log(win);
+     } else if (humanChoice === "paper" && computerChoice === "scissors"){
+     ++computerScore;
+     console.log(lose);
+     } else if (humanChoice === "paper" && computerChoice === "rock"){
+     ++humanScore;
+     console.log(win);
+     } else if (humanChoice === "scissors" && computerChoice === "rock"){
+     ++computerScore;
+     console.log(lose);
+     } else if (humanChoice === "scissors" && computerChoice === "paper"){
+     ++humanScore;
+     console.log(win);
+     } else {
+     console.log("It's a tie")
+     }
+
+     //Prints the score to the console after each round
+     console.log(`You: ${humanScore} `+ ` Computer: ${computerScore}`);
+    }
+}
