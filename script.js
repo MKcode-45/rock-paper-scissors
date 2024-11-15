9f65c8759eb02596d2ee7f44d61fec23be7ce43f
MKcode-45 <kalotamishael@gmail.com>
2024-11-15 19:46:05 +0200

    Added UI to the project
    Using event listeners a user interface has been
    added to the game.

--- script.js
+++ script.js
@@ -1,88 +1,53 @@
-//Created function to get computer choice
-//Function will choose three random numbers which are equal to rock paper or scissors
 function getComputerChoice(){
-    let computerChoice = Math.floor(Math.random()*3);
-    if(computerChoice === 0){
-      return "rock";
-    }
-    if(computerChoice === 1){
-      return "paper";
-    }
-   if(computerChoice === 2){
-      return "scissors";
-    }
-}
-  
-//A function that takes the human choice
-function getHumanChoice(){
-    let result = prompt("To play, choose rock, paper or scissors").toLocaleLowerCase();
-    return result;
+  const computerOptions = ["rock", "paper", "scissors"];
+  return computerOptions[Math.floor(Math.random()*computerOptions.length)];
 }
 
-//Game play function
-function playGame(){
+//The two variables initialize human and computer scores
 
-    //The two variables initialize human and computer scores
+  let humanScore = 0;
+  let computerScore = 0;
 
-    let humanScore = 0;
-    let computerScore = 0;
-
-    //Function runs a single round of the game
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
     
- function playRound(humanChoice, computerChoice) {
-  
-   var win = `You win! ${humanChoice} beats ${computerChoice}`;
-   var lose = `You lose! ${computerChoice} beats ${humanChoice}`;
+    button.addEventListener("click", (event) => {
+    let humanChoice = event.target.id;
 
-   //The conditions determine the winner of each round
-   //Score is increased depending on the winner
-   
-   if (humanChoice === "rock" && computerChoice === "paper"){
-     ++computerScore;
-     console.log(lose);
-     } else if (humanChoice === "rock" && computerChoice === "scissors"){
-     ++humanScore;
-     console.log(win);
-     } else if (humanChoice === "paper" && computerChoice === "scissors"){
-     ++computerScore;
-     console.log(lose);
-     } else if (humanChoice === "paper" && computerChoice === "rock"){
-     ++humanScore;
-     console.log(win);
-     } else if (humanChoice === "scissors" && computerChoice === "rock"){
-     ++computerScore;
-     console.log(lose);
-     } else if (humanChoice === "scissors" && computerChoice === "paper"){
-     ++humanScore;
-     console.log(win);
-     } else {
-     console.log("It's a tie")
-     }
+    function playRound(humanChoice, computerChoice) {
 
-     //Prints the score to the console after each round
-     console.log(`You: ${humanScore} `+ ` Computer: ${computerScore}`);
-    }
-
-// playRound function is called five times to play five rounds
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-
- //champion function anounces the winner after all five rounds are played
- //It compares the two score variables to determine the winner
-
-    function champion(){
-        if (humanScore > computerScore){
-            console.log("WELL DONE YOU WIN!");
-        } else if(humanScore === computerScore){
-            console.log("It's a tie reload to play again.")
+      if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock"){
+        ++computerScore;
+      } else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper"){
+        ++humanScore;
+      } else {
+          computerScore;
+          humanScore;
         }
-        else {console.log("SORRY, YOU LOOSE.");}
+
+        const choice = document.querySelector("#choice");
+        choice.firstElementChild.textContent = `You chose ${humanChoice}, the computer chose ${computerChoice}.`;
+        
+        const gameScore = document.querySelector("#scores");
+        gameScore.firstElementChild.textContent = `Player: ${humanScore}`;
+        gameScore.lastElementChild.textContent = `Computer: ${computerScore}`;
+
+        const gameWinner = document.querySelector(".final-score");
+
+        if(humanScore === 5){
+          gameWinner.firstElementChild.textContent = "Congragulations you win!";
+        } 
+
+        if(computerScore === 5){
+          gameWinner.firstElementChild.textContent = "Sorry you lose.";      
+        }
     }
-   
-    champion();
- }
- 
- playGame();
+
+       if(computerScore === 5 || humanScore === 5){
+        return;      
+        }
+
+       playRound(humanChoice, getComputerChoice());
+    });
+  });
+
